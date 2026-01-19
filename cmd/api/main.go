a09df6db45b3dbee032409255c7141fd3403dddc
jonathancolombo <jonathancolombo98@outlook.com>
2026-01-19 22:19:11 +0100

    Added devcontainer

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,21 +4,16 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	env := os.Getenv("APP_ENV")
 	if env == "" {
+		log.Println("APP_ENV env variable not set")
 		env = "develop"
 	}
-	err := godotenv.Load(".env/" + env + ".env")
-	if err != nil {
-		log.Printf("No .env file found for %s environment", env)
-	}
-
+	log.Println("APP_ENV env:", env)
 	log.Println("🚆 Railway Ticketing System API starting...")
 
 	http.HandleFunc("/health", func(responseWriter http.ResponseWriter, request *http.Request) {
@@ -30,7 +25,7 @@ func main() {
 	})
 
 	address := "0.0.0.0:8080"
-	log.Printf("Server running on http://localhost%s\n\n", address)
+	log.Printf("Server running on http://%s\n\n", address)
 
 	if err := http.ListenAndServe(address, nil); err != nil {
 		log.Fatal(err)
